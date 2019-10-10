bff3030893a3e0033d3046dbfb981b1672945b86
Max J. Polster <max.polster@id-p.de>
2019-10-10 19:55:21 +0200

    Support building ts files.

--- src/tooling/runners/library/index.ts
+++ src/tooling/runners/library/index.ts
@@ -1,7 +1,29 @@
+import asyncDone = require("async-done");
+import { src, dest } from "vinyl-fs";
+import Vinyl = require("vinyl");
+import merge = require("merge-stream");
+import { createProject } from "gulp-typescript";
 import { VcConfig } from "../../config";
 import { VcRunnerContext } from "..";
+import { join } from "path";
 
 export async function run(config: VcConfig, context: VcRunnerContext) {
-	throw new Error("Not implemented.");
+	const ts = createProject(join(config.context, "tsconfig.json"));
+	const tsProcessor = ts();
+
+	await new Promise((resolve, reject) => {
+		asyncDone(() => {
+			return src(config.rootDir + "/**/*.ts")
+				.pipe(tsProcessor)
+				.pipe(dest(config.outDir));
+		}, (error) => {
+			if (error) {
+				reject(error);
+			} else {
+				resolve();
+			}
+		});
+	});
+
 	// TODO: Build sources using gulp.
 }
