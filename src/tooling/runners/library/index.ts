9e85c211560c2c82e196794fff67dbed0c918c5c
Max J. Polster <Max.Polster@netatwork.de>
2019-10-11 11:12:42 +0200

    Custom ts error handling.

--- src/tooling/runners/library/index.ts
+++ src/tooling/runners/library/index.ts
@@ -1,29 +1,38 @@
-import asyncDone = require("async-done");
 import { src, dest } from "vinyl-fs";
 import Vinyl = require("vinyl");
 import merge = require("merge-stream");
-import { createProject } from "gulp-typescript";
+import ts = require("gulp-typescript");
 import { VcConfig } from "../../config";
 import { VcRunnerContext } from "..";
 import { join } from "path";
+import { formatTsError } from "./ts-util";
 
 export async function run(config: VcConfig, context: VcRunnerContext) {
-	const ts = createProject(join(config.context, "tsconfig.json"));
-	const tsProcessor = ts();
+	const tsProject = ts.createProject(join(config.context, "tsconfig.json"));
+	const tsProcessor = tsProject({
+		error(error) {
+			console.error(formatTsError(error));
+		}
+	});
 
-	await new Promise((resolve, reject) => {
-		asyncDone(() => {
-			return src(config.rootDir + "/**/*.ts")
-				.pipe(tsProcessor)
-				.pipe(dest(config.outDir));
-		}, (error) => {
-			if (error) {
-				reject(error);
+	// TODO: Support ts incremental compilation in watch mode.
+
+	await stream(reject => src(config.rootDir + "/**/*.ts")
+		.pipe(tsProcessor.on("error", reject))
+		.pipe(dest(config.outDir))
+	);
+}
+
+function stream(start: (reject: (error: any) => void) => NodeJS.ReadWriteStream) {
+	return new Promise((resolve, reject) => {
+		const errors: any[] = [];
+		function done() {
+			if (errors.length > 0) {
+				reject(errors[0]);
 			} else {
 				resolve();
 			}
-		});
+		}
+		start(error => void errors.push(error)).on("end", done).on("close", done);
 	});
-
-	// TODO: Build sources using gulp.
 }
---
+++ src/tooling/runners/library/ts-util.ts
@@ -0,0 +1,22 @@
+import { DiagnosticCategory } from "typescript";
+import ts = require("gulp-typescript");
+import colors = require("ansi-colors");
+
+// The following logging behavior is the same as in ts-loader (see npm) to
+// provide a bit more consistency between different build systems (gulp and webpack).
+const categories = new Map<DiagnosticCategory, { style: colors.StyleFunction, label: string }>([
+	[DiagnosticCategory.Error, { style: colors.redBright, label: "ERROR" }],
+	[DiagnosticCategory.Message, { style: colors.green, label: "MESSAGE" }],
+	[DiagnosticCategory.Warning, { style: colors.yellow, label: "WARNING" }]
+]);
+
+export function formatTsError(error: ts.reporter.TypeScriptError) {
+	const category = categories.get(error.diagnostic.category);
+	if (category) {
+		return category.style(`[ts] ${category.label} in ${colors.cyanBright(`${error.fullFilename}${error.startPosition ? `(${formatPosition(error.startPosition)})` : ""}`)}\n     TS${error.diagnostic.code}: ${error.diagnostic.messageText}`);
+	}
+}
+
+function formatPosition({ line, character }: { line: number, character: number }) {
+	return `${line},${character}`;
+}
