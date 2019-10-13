0160c31cce82352c11a00a69cd1868a3fed31880
Max J. Polster <max.polster@id-p.de>
2019-10-13 11:31:55 +0200

    Declare webpack require.context

--- src/runtime/types.ts
+++ src/runtime/types.ts
@@ -3,3 +3,15 @@ declare module "*.vue" {
 	import Vue from "vue";
 	export default Vue;
 }
+
+interface NodeRequire {
+	context: (
+		directory: string,
+		includeSubdirs?: boolean,
+		filter?: RegExp,
+		mode?: string
+	) => ((id: string) => any) & {
+		resolve(id: string): string;
+		keys(): string[];
+	};
+}
